4c5d4d4e02bc5bd683e1f20064d41ad73e0d3002
sanleriver-IAS <sanleriver@gmail.com>
2022-03-22 16:25:59 -0500

    creando el repositorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
+import { PiePaginaComponent } from './components/pie-pagina/pie-pagina.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { FormularioUsuarioComponent } from './components/formulario-usuario/formulario-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPrincipalComponent,
+    PiePaginaComponent,
+    UsuarioComponent,
+    FormularioUsuarioComponent
   ],
   imports: [
     BrowserModule,
